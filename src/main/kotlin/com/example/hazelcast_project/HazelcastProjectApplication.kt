eeacf9c9746a653e77c572f3e9e8e848d78df6d3
Ange <leyre.ange@gmail.com>
2025-10-29 14:50:03 +0100

    fix : build error

--- src/main/kotlin/com/example/hazelcast_project/HazelcastProjectApplication.kt
+++ src/main/kotlin/com/example/hazelcast_project/HazelcastProjectApplication.kt
@@ -4,19 +4,20 @@ import com.hazelcast.core.Hazelcast
 import com.hazelcast.map.IMap
 import java.io.Serializable
 import com.hazelcast.query.Predicates
+import com.hazelcast.query.Predicate
 
 fun main(args: Array<String>) {
     println("Démarrage...")
     
     val hazelcastInstance1 = Hazelcast.newHazelcastInstance()
-    val peopleMap: IMap<String, Person> = hazelcastInstance1.getMap("ma-people-map")
+    val peopleMap: IMap<String , Person> = hazelcastInstance1.getMap("ma-people-map")
     
     peopleMap.put("p1", Person("Alpha", 25, "Paris"))
     peopleMap.put("p2", Person("Bravo", 35, "Lyon"))
     peopleMap.put("p3", Person("Charlie", 40, "Paris"))
     
     println("Création du filtre SQL : age >30")
-    val filtreAge = Predicates.sql("age > 30")
+    val filtreAge : Predicate<String, Person> = Predicates.sql("age > 30")
     val resultats = peopleMap.values(filtreAge)
     
     println("--- Personnes de plus de 30 ans ---")
