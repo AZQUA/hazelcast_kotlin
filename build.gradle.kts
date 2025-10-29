03fcdf64629308e7165d7d1aeef0c1bf2ae3b141
Ange <leyre.ange@gmail.com>
2025-10-29 11:58:41 +0100

    exo 8

---
+++ build.gradle.kts
@@ -0,0 +1,39 @@
+plugins {
+	kotlin("jvm") version "1.9.25"
+	id("application")
+}
+
+// Remplace "com.example.hazelcastproject.HazelcastProjectApplicationKt"
+// par le vrai nom de ton fichier principal.
+// Astuce : c'est souvent "NomDuFichierKt".
+// Regarde dans src/main/kotlin/... pour trouver le nom de ton fichier .kt
+application {
+    mainClass.set("com.example.hazelcast_project.HazelcastProjectApplicationKt")
+}
+
+group = "com.example"
+version = "0.0.1-SNAPSHOT"
+description = "Demo project for Spring Boot"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(17)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	implementation("com.hazelcast:hazelcast:5.3.6")
+	implementation("org.jetbrains.kotlin:kotlin-stdlib")
+	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+}
+
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ src/main/kotlin/com/example/hazelcast_project/HazelcastProjectApplication.kt
@@ -0,0 +1,34 @@
+package com.example.hazelcast_project
+
+import com.hazelcast.core.Hazelcast
+import com.hazelcast.map.IMap
+import java.io.Serializable
+import com.hazelcast.query.Predicates
+
+fun main(args: Array<String>) {
+    println("Démarrage...")
+    
+    val hazelcastInstance1 = Hazelcast.newHazelcastInstance()
+    val peopleMap: IMap<String, Person> = hazelcastInstance1.getMap("ma-people-map")
+    
+    peopleMap.put("p1", Person("Alpha", 25, "Paris"))
+    peopleMap.put("p2", Person("Bravo", 35, "Lyon"))
+    peopleMap.put("p3", Person("Charlie", 40, "Paris"))
+    
+    println("Création du filtre SQL : age >30")
+    val filtreAge = Predicates.sql("age > 30")
+    val resultats = peopleMap.values(filtreAge)
+    
+    println("--- Personnes de plus de 30 ans ---")
+    resultats.forEach { person ->
+        println(person)
+    }
+    
+    hazelcastInstance1.shutdown()
+}
+
+data class Person(
+    val name: String,
+    val age: Int,
+    val city: String
+) : Serializable
