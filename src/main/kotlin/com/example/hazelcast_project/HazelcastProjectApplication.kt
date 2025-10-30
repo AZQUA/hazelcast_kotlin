7e5501b85acf5fe15a33bf6cf1305fd36a4676bb
Ange <leyre.ange@gmail.com>
2025-10-30 14:49:13 +0100

    exo_7

--- src/main/kotlin/com/example/hazelcast_project/HazelcastProjectApplication.kt
+++ src/main/kotlin/com/example/hazelcast_project/HazelcastProjectApplication.kt
@@ -6,31 +6,31 @@ import com.hazelcast.collection.IList
 import com.hazelcast.collection.ISet
 import com.hazelcast.collection.IQueue
 import kotlinx.coroutines.*
-import com.hazelcast.core.EntryListener
-import com.hazelcast.core.EntryAdapter
-import com.hazelcast.core.EntryEvent 
+import com.hazelcast.topic.ITopic
+import java.util.EventListener
 
 
 fun main(args: Array<String>) {
     println("Démarrage...")
     val hazelcastInstance1 = Hazelcast.newHazelcastInstance()
-    val mapListener: IMap<String, String> = hazelcastInstance1.getMap("ma-map")
-    mapListener.addEntryListener(object : EntryAdapter<String,String>() {
-        override fun entryAdded(event: EntryEvent<String, String>) {
-            val timestampActuel = java.util.Date()
-            println("Listener Donnée AJOUTEE : Clé=${event.key}, Valeur=${event.value}, Date=${timestampActuel}")
+    val Topic: ITopic<String> = hazelcastInstance1.getTopic("mon-topic")
+    
+    Topic.addMessageListener(object : MessageListener<String> {
+        override fun onMessage(message: Message<String>) {
+            println("[Abonné A] a reçu : ${message.messageObject}")
         }
-        override fun entryUpdated(event: EntryEvent<String, String>) {
-            val timestampActuel = java.util.Date()
-            println("LISTENER Donnée MISE A JOUR : Clé=${event.key}, NouvelleValeur=${event.value}, Date=${timestampActuel}")
+    })
+    Topic.addMessageListener(object : MessageListener<String> {
+        override fun onMessage(message: Message<String>) {
+            println("[Abonné B] a reçu : ${message.messageObject}")
         }
-        override fun entryRemoved(event: EntryEvent<String, String>) {
-            val timestampActuel = java.util.Date()
-            println("LISTENER Donnée SUPPRIMEE : Clé=${event.key}, Date=${timestampActuel}")
+    })
+    Topic.addMessageListener(object : MessageListener<String> {
+        override fun onMessage(message: Message<String>) {
+            println("[Abonné C] a reçu : ${message.messageObject}")
         }
-    }, true)
+    })
+    println("\n--- Le Publisher envoie un message ! ---")
+    Topic.publish("Bonjour tout le monde !")
 
-    mapListener.put("cle-1","donnee-1")
-    mapListener.put("cle-1","donnee-1-modif")
-    mapListener.remove("cle-1")
 }
