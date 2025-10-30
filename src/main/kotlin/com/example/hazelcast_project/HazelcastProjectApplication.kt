53723ef0b75446684a4f2f23fe266aac26a465fb
Ange <leyre.ange@gmail.com>
2025-10-30 09:57:21 +0100

    exo_5

--- src/main/kotlin/com/example/hazelcast_project/HazelcastProjectApplication.kt
+++ src/main/kotlin/com/example/hazelcast_project/HazelcastProjectApplication.kt
@@ -4,17 +4,28 @@ import com.hazelcast.core.Hazelcast
 import com.hazelcast.map.IMap
 import com.hazelcast.collection.IList
 import com.hazelcast.collection.ISet
+import com.hazelcast.collection.IQueue
+import kotlinx.coroutines.*
 
 
-fun main(args: Array<String>) {
+fun main(args: Array<String>) = runBlocking {
     println("Démarrage...")
     val hazelcastInstance1 = Hazelcast.newHazelcastInstance()
-    val hazelcastInstance2 = Hazelcast.newHazelcastInstance()
-    val maMap1: IMap<String, String> = hazelcastInstance1.getMap("ma-Map")
-    val maMap2: IMap<String, String> = hazelcastInstance2.getMap("ma-Map")
-    maMap1.put("test-cle","donne-instance-1")
-    val valeurLue = maMap2.get("test-cle")
-    println(valeurLue)
+    val maQueue:IQueue<String> = hazelcastInstance1.getQueue("ma-queue")
+    launch {
+        for (i in 1..20) {
+            maQueue.put("Message ${i}")
+            println("Producteur : à envoyé un 'Message ${i}'.")
+            delay(100)
+        }
+    }
+    launch {
+        for (i in 1..20) {
+            val message = maQueue.take()
+            println("Consomateur : à reçu un '$message'")
+            delay(100)
+        }
+    }
+    delay(20000)
     hazelcastInstance1.shutdown()
-    hazelcastInstance2.shutdown()
 }
