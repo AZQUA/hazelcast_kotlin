9f5daa1900522011f2e9d61cbdf105e65b7844d8
Ange <leyre.ange@gmail.com>
2025-10-30 16:54:32 +0100

    exo_10

--- src/main/kotlin/com/example/hazelcast_project/HazelcastProjectApplication.kt
+++ src/main/kotlin/com/example/hazelcast_project/HazelcastProjectApplication.kt
@@ -3,20 +3,29 @@ package com.example.hazelcast_project
 import com.hazelcast.core.Hazelcast
 import com.hazelcast.map.IMap
 import com.example.hazelcast_project.IncrementingProcessor
+import com.hazelcast.core.IExecutorService
+import java.io.Serializable
+import java.util.concurrent.Callable
 
 fun main(args: Array<String>) {
     println("Démarrage...")
     val hazelcastInstance1 = Hazelcast.newHazelcastInstance()
-    val countersMap: IMap<String, Int> = hazelcastInstance1.getMap("compteur")
-    
-    countersMap.put("monCompteur",0)
-    println("Compteur initialisé à : ${countersMap.get("monCompteur")}")
+    val monExecutor: IExecutorService = hazelcastInstance1.getExecutorService("Mon-executor")
+    val futureResultat = monExecutor.submit( MaTacheSerializable() )
 
-    println("Envoi de l'EntryProcesor pour incrémenter...")
-    val resultat = countersMap.executeOnKey("monCompteur", IncrementingProcessor())
-    println("Le processeur à retourné la nouvelle valeur : $resultat")
-    println("Verification dans la map : ${countersMap.get("monCompteur")}")
+    // 2. Attendre et réclamer le résultat
+    println("En attente du résultat...")
+    val resultat = futureResultat.get() // <-- Cette ligne va bloquer jusqu'à ce que la tâche soit finie
 
+    println("Le membre distant a répondu : $resultat")
     hazelcastInstance1.shutdown()
 }
 
+class MaTacheSerializable : Callable<String>, Serializable {
+    override fun call(): String {
+        // Code exécuté sur le membre distant
+        println("--- Je m'exécute sur un membre du cluster ! ---")
+        Thread.sleep(1000) // Simule un long calcul
+        return "Le calcul est terminé !"
+    }
+}
