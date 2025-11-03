6ddee78c124d011a37ead09ff071e1ca2f81a359
Ange <leyre.ange@gmail.com>
2025-11-03 10:22:59 +0100

    exo_11

--- src/main/kotlin/com/example/hazelcast_project/HazelcastProjectApplication.kt
+++ src/main/kotlin/com/example/hazelcast_project/HazelcastProjectApplication.kt
@@ -14,40 +14,50 @@ fun main(args: Array<String>) = runBlocking {
     println("Démarrage...")
     val hazelcastInstance1 = Hazelcast.newHazelcastInstance()
     val verrou : FencedLock = hazelcastInstance1.cpSubsystem.getLock("mon-verrou")
+    val threadA = newSingleThreadContext("lock-A")
+    val threadB = newSingleThreadContext("lock-B")
     val jobA = launch {
         println("Tentative de lock de l'utilisateur A")
-        if (verrou.tryLock(2, TimeUnit.SECONDS)) {
-            try {
-                println("J'ai le verrou ! je fais mon travail critique...")
-                delay(5000)
-                println("J'ai fini mon travail.")
-            } finally {
-                verrou.unlock()
-                println("Verrou rendu par l'utilisateur A")
+        withContext(threadA) {
+        val acquired = verrou.tryLock(2, TimeUnit.SECONDS)
+            if (acquired) {
+                try {
+                    println("J'ai le verrou ! je fais mon travail critique...")
+                    delay(5000)
+                    println("J'ai fini mon travail.")
+                } finally {
+                    verrou.unlock()
+                    println("Verrou rendu par l'utilisateur A")
+                }
             }
-        }
-        else {
-            println("Timeout dépassé, abandon")
+            else {
+                println("Timeout dépassé, abandon")
+            }
         }
     }
     val jobB = launch {
         println("Tentative de lock de l'utilisateur B")
-        if (verrou.tryLock(2, TimeUnit.SECONDS)) {
-            try {
-                println("J'ai le verrou ! je fais mon travail critique...")
-                delay(5000)
-                println("J'ai fini mon travail.")
-            } finally {
-                verrou.unlock()
-                println("Verrou rendu par l'utilisateur B")
+        withContext(threadB) {
+            val acquired = verrou.tryLock(2, TimeUnit.SECONDS)
+            if (acquired) {
+                try {
+                    println("J'ai le verrou ! je fais mon travail critique...")
+                    delay(5000)
+                    println("J'ai fini mon travail.")
+                } finally {
+                    verrou.unlock()
+                    println("Verrou rendu par l'utilisateur B")
+                }
             }
-        }
-        else {
-            println("Timeout dépassé, abandon")
+            else {
+                println("Timeout dépassé, abandon")
+            }
         }
     }
     jobA.join()
     jobB.join()
+    threadA.close()
+    threadB.close()
     hazelcastInstance1.shutdown()
 }
 
